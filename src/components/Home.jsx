804aecc6f5ee7e825f6a64c3477b43bccb0f91c7
Uh <118338842+mayoi-ayase@users.noreply.github.com>
2024-03-21 15:03:45 +1100

    vercel fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="home-wrapper">
       <h1 className="home-header">Welcome to the Metropolitan Museum of Art</h1>
-      <Link className="home-link" to="/paintings">
+      <Link reloadDocument className="home-link" to="/paintings">
         I just want to have a look around. {<ChevronRight />}
       </Link>
       {/* ^if i don't have reloadDocument sometimes the page loads without calling the API, it's easier to just have the page reload each time you want to look at the RNG page. this comes at the sacrifice of dark/lightmode resetting but it's easier for an end user to re-toggle the darkmode button than know to refresh the page... */}
